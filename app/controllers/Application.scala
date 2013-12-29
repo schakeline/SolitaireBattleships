75d7e1f0e3d1968565a6c112f515bab214af4dc3
th0m2y <thomasbopst@web.de>
2013-12-29 14:42:59 +0100

    Added showSolution view and Exceptionhandling for placeShip and removeShip

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,6 +7,7 @@ import play.api.data.Forms._
 import de.htwg.scala.solitairebattleship.controller.GameController
 import de.htwg.scala.solitairebattleship.util.Orientation
 import de.htwg.scala.solitairebattleship.model.Model
+import de.htwg.scala.solitairebattleship.model.exception.ShipCollisionException
 
 case class PlaceShipData(id: Int, x: String, y:String, orientation:String)
 
@@ -42,7 +43,7 @@ object Application extends Controller {
       size => {
         //Model create game
         gameController.newGame(size)
-        Ok( views.html.playGame(placeShipForm, removeShipForm) )
+        Ok( views.html.playGame(placeShipForm, removeShipForm, None, false) )
       }
     )
   }
@@ -51,28 +52,51 @@ object Application extends Controller {
     implicit request =>
       placeShipForm.bindFromRequest.fold(
         formWithErrors => {
-          BadRequest(views.html.playGame(placeShipForm, removeShipForm))
+          BadRequest(views.html.playGame(placeShipForm, removeShipForm, Some("Bad Request"), false))
         },
         placeShipData => {
           val orientation = if(placeShipData.orientation == "Horizontal") Orientation.Horizontal else Orientation.Vertical
           val x:Int = placeShipData.x(0).toInt-65
           val y:Int = placeShipData.y(0).toInt-65
-          gameController.placeShip(placeShipData.id, x, y, orientation)
-          println("OK")
-          Ok( views.html.playGame(placeShipForm, removeShipForm) )
+          
+          var errorMsg:Option[String] = None
+          try {
+            gameController.placeShip(placeShipData.id, x, y, orientation)
+          }
+          catch {
+            case e:IllegalArgumentException =>
+              errorMsg = Some("Illegal argument")
+            case e:IndexOutOfBoundsException =>
+              errorMsg = Some("Index out of bounds.")
+            case e:ShipCollisionException =>
+              errorMsg = Some("ShipCollition: " + e.getMessage)
+          }
+          
+          Ok( views.html.playGame(placeShipForm, removeShipForm, errorMsg, false) )
         }
       )
   }
 
   def removeShip = Action { implicit request =>
     removeShipForm.bindFromRequest.fold(
-      errors => BadRequest(views.html.playGame(placeShipForm, removeShipForm)),
+      errors => BadRequest(views.html.playGame(placeShipForm, removeShipForm, Some("Illegal argument"), false)),
       id => {
         //Model create game
-        gameController.removeShip(id)
-        Ok( views.html.playGame(placeShipForm, removeShipForm) )
+        var errorMsg:Option[String] = None
+        try {
+          gameController.removeShip(id)
+        }
+        catch {
+          case e:IllegalArgumentException => 
+            errorMsg = Some("Illegal argument")
+        }
+        Ok( views.html.playGame(placeShipForm, removeShipForm, errorMsg, false) )
       }
     )
+  }
+
+  def showSolution = Action {
+    Ok(views.html.showSolution())
   }
 
 }
